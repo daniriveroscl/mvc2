ee86a31e46ac2ebf8ec10f6922522f7047462166
Daniel Riveros <danielriveroscorrea@gmail.com>
2022-11-24 09:12:59 +0000

    bla

---
+++ app/models/Product.php
@@ -0,0 +1,18 @@
+<?php
+namespace App\Models;
+
+use PDO;
+use Core\Model;
+
+require_once '../core/Model.php';
+/**
+*
+*/
+class Product extends Model
+{
+    public static function all(){  }
+    public static function find($id){  }
+    public function insert(){  }
+    public function delete(){  }
+    public function save(){  }
+}
--- core/Model.php
+++ core/Model.php
@@ -1,14 +1,19 @@
 <?php
 
 namespace Core;
+use PDO;
+use PDOException;
+
+require_once '../config/db.php';
+
+use const Config\DSN;
+use const Config\USER;
+use const Config\PASSWORD;
 
 class Model {
     protected static function db(){
-        $dsn = "mysql:dbname=mvc;host=db";
-        $usuario = "root";
-        $contraseña = "password";
         try {
-            $db = new PDO($dsn, $usuario, $contraseña);
+            $db = new PDO(DSN, USER, PASSWORD);
             $db ->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e){
             echo 'Falló la conexión: ' . $e->getMessage();
